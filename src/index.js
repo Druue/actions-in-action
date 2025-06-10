acbbe53a40f71a36abe86d099403e859bfc49735
Sophie Atkins <sophie.atkins@mentimeter.com>
2025-06-10 13:13:31 +0200

    dispatch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import { Octokit } from "octokit";
+
+const url = "https://api.github.com/repos/druue/actions-in-action/dispatches";
+
+const octokit = new Octokit();
+
+const resp = octokit.request("POST /repos/{owner}/{repo}/dispatches", {
+  owner: "Druue",
+  repo: "actions-in-action",
+  event_type: "event1",
+  client_payload: {
+    passed: false,
+    message: "Error: timeout",
+  },
+});
+
+console.log(resp);
